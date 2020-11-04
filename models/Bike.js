8fac92840bbf6c156863296a440b8ec442f26b00
Jhonatan C Hurtado <jhonatanchurtado@gmail.com>
2020-11-03 22:21:10 -0500

    Agregando métodos al modelo Bike y generando .gitignore para evitar subir la carpeta node_modules al repositorio

---
+++ models/Bike.js
@@ -0,0 +1,41 @@
+// Modelo
+var Bike = function(id,modelo,color,ubicacion){
+    this.id = id;
+    this.modelo = modelo;
+    this.color = color;
+    this.ubicacion = ubicacion;
+}
+
+// Arreglo para guardar información
+Bike.arrayBikes = [];
+
+// Agregar una nueva bicicleta al arreglo
+Bike.add = function(data){
+    Bike.allBike.push(data);
+}
+
+// Buscar un elemento
+Bike.searchBike = function(id){
+    // Buscar y obtener ID dentro del array, y luego igualarlo al valor que se recibe por el parámetro
+    let BikeID = Bike.arrayBikes.find( element => element.id == id );
+
+    // Si existe el ID, returnarlo
+    if(BikeID){
+        return BikeID;
+    } else {
+        // Si no existe retornar el error con el número del ID
+        throw new Error(`No existe una bicicleta con el id ${BikeID}`);
+    }
+}
+
+// Eliminar un elemento
+Bike.removeBike = function(id){
+    let BikeID = Bike.arrayBikes.find( element => element.id == id );
+    if(BikeID){
+        Bike.allBike.splice(i, 1);
+    } else {
+        throw new Error(`No existe una bicicleta con el id ${BikeID}`);
+    }
+}
+
+module.exports = Bike;
